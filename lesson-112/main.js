b06de5ae5dcd177893cbf7dcfb7424948716ab47
PepaHub <nickminaylov@gmail.com>
2025-03-12 14:37:43 +0300

    Add files via upload

---
+++ lesson-112/main.js
@@ -0,0 +1,32 @@
+const btnOpen = document.querySelector('.btn-open');
+const modal = document.querySelector('.modal');
+const body = document.body
+
+const closeModal = () => {
+    modal.classList.remove('modal--open');
+    body.classList.remove('body--fixed');
+}
+
+const openModal = () => {
+    modal.classList.add('modal--open');
+    body.classList.add('body--fixed');
+}
+
+btnOpen.addEventListener('click', () => {
+    openModal();
+});
+
+modal.addEventListener('click', (event) => {
+    const target = event.target
+    if (target && target.classList.contains('modal') || target.classList.contains('modal__close-btn')) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' && modal.classList.contains('modal--open')) {
+        closeModal();
+    }
+});
+
+
